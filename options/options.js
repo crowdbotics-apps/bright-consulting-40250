3b2aa646e0b3b5c1814e5364ad6225147daf590a
Crowdbotics <team@crowdbotics.com>
2023-04-27 04:17:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bright-consulting-40250",
+  url: "https://bright-consulting-40250.botics.co",
+  api: "https://bright-consulting-40250.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
